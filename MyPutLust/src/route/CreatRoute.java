9ad67b48f2eb3f2027a69170d118fbaaa42edd71
Aleksandrman <35749767+Aleksandrman@users.noreply.github.com>
2018-04-02 19:17:02 +0800

    home

--- MyPutLust/src/route/CreatRoute.java
+++ MyPutLust/src/route/CreatRoute.java
@@ -1,5 +1,19 @@
 package route;
 
-public class CreatRoute {
+import java.util.ArrayList;
 
+public class CreatRoute {
+	int benz = 1;
+	int bak = 30;
+	
+public CreatRoute() {
+	ListRoute ls = new ListRoute();
+	ArrayList<String> route = ls.route;
+	
+	for (String string : route) {
+		
+	}
+	
+	
+}
 }
--- MyPutLust/src/route/ListRoute.java
+++ MyPutLust/src/route/ListRoute.java
@@ -1,5 +1,18 @@
 package route;
 
+import java.util.ArrayList;
+
 public class ListRoute {
+ArrayList<String> route= new ArrayList<>();
+
+
+public ListRoute() {
+	route.add("1|40|20");
+	route.add("2|20|10");	
+	route.add("3|30|30");
+	route.add("4|50|50");
+	route.add("5|60|10");
+	
+}
 
 }
